0d14fe9201838db36503e4fc3289ed0aab809820
lepronet85 <lepronet85@gmail.com>
2023-12-17 18:30:10 +0100

    Update files

--- graphql/builder.ts
+++ graphql/builder.ts
@@ -2,11 +2,15 @@ import SchemaBuilder from "@pothos/core";
 import PrismaPlugin from "@pothos/plugin-prisma";
 import type PrismaTypes from "@pothos/plugin-prisma/generated";
 import prisma from "../lib/prisma";
+import RelayPlugin from "@pothos/plugin-relay";
+import { createContext } from "./context";
 
 export const builder = new SchemaBuilder<{
   PrismaTypes: PrismaTypes;
+  Context: ReturnType<typeof createContext>;
 }>({
-  plugins: [PrismaPlugin],
+  plugins: [PrismaPlugin, RelayPlugin],
+  relayOptions: {},
   prisma: {
     client: prisma,
   },
